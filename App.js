8f3188978434396f5003ad75a3bab60003d216ce
Mutuon Joan <mutuon.j@gmail.com>
2024-09-30 00:07:19 +0200

    test

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = React.useState(0);
+
+  const onPress = () => setCount(count + 1);
+
+  useEffect(() => {
+
+  })
   return (
+
+
     <View style={styles.container}>
-      <Button title="Increment" />
+      <Button title="Increments" onPress={onPress} />
       <StatusBar style="auto" />
     </View>
   );
